e5f564bfaa6870abcb12935b3060a2b48477da96
Roman Zeldinov <74790793+RomanZSeldinov@users.noreply.github.com>
2024-02-15 20:55:25 +0300

    Create sysinfo.py

---
+++ sysinfo.py
@@ -0,0 +1,66 @@
+import io
+def checkos():
+    try:
+        with open("etc/kernelver") as file:
+            return "NACO"
+    except IOError:
+        try:
+          with open("etc/monacover") as monacofile:
+            return "Monaco"
+        except IOError:
+          return "Other"
+
+def sysinfo():
+    os = checkos()
+    osver = ""
+    term = ""
+    if (os == "NACO"):
+        try:
+            import lib.sysinfo as sysinfo
+            osver = sysinfo.sysver()
+            term = sysinfo.term()
+            print("""
+          ((((((         ((((((         
+          ((((((((       ((((((         
+          (((((((((      ((((((         
+          (((((((((((    ((((((         
+          ((((((((((((   ((((((         
+          (((((((((((((  ((((((         
+          (((((((((((((((((((((         
+          (((((((((((((((((((((         
+          (((((((((((((((((((((         
+          (((((((((((((((((((((         
+          (((((((((((((((((((((         
+          ((((((  (((((((((((((         
+          ((((((   ((((((((((((         
+          ((((((     ((((((((((         
+          ((((((      (((((((((         
+          ((((((       ((((((((                    
+                  """)
+            print("OS: " + os)
+            print("Version: " + osver)
+            print("Terminal: " + term)
+        finally:
+            pass
+    elif (os == "Monaco"):
+        print("""
+                                                      
+                                        
+                                        
+                  *****                  
+                  @@@@@                 
+*****            @@@@@@@            ****
+  @@@          @@@@@@@@@@@          @@@ 
+   @@@@      @@@@@@@@@@@@@@@      @@@@  
+   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   
+                                        
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
+                                        
+                                        
+              """)
+        print("OS: " + os)
+    else:
+      print("OS: " + os)
